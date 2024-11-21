b2ab1f7a4bbab6a0a833c211b749befc575a993d
ahmed-n-abdeltwab <ahmed.n.abdeltwab@gmail.com>
2024-11-21 16:12:49 +0200

    setting my project: 1- add gitignore 2- add prettier 3- init typescript

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+
+console.log("hello world");
+// const express =  'express';
+// const app = express();
+
+// // product router
+// import imageRouter from './routes/imageRoutes';
+
+// // error handlers
+// import notFoundMiddleware from './middlewares/not-found.middleware';
+// import errorHandlerMiddleware from './middlewares/error.middleware';
+
+// // logger
+// import Logger from './middlewares/logger.middleware';
+
+// // logger
+// import cacheMiddleware from './middlewares/cache.middleware';
+
+// // middleware
+// app.use(express.json());
+// app.use(Logger);
+// app.use(cacheMiddleware);
+
+// app.get('/', (req: express.Request, res: express.Response): void => {
+// 	res.status(200).send(
+// 		'<h1>Image Processing API</h1><a href="/api/images">images route</a>'
+// 	);
+// });
+// app.use('/api/images', imageRouter);
+
+// app.use(notFoundMiddleware);
+// app.use(errorHandlerMiddleware);
+
+// const port = 5000;
+
+// app.listen(port, (): void =>
+// 	console.log(`Server is listening at http://localhost:${port}`)
+// );
+
+// export default app;
